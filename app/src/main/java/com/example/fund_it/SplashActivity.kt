a9dcc6e29952766ccb73460bbad59baed11b7953
Vishwas <VishwasDubey150@gmail.com>
2023-02-15 09:05:02 +0530

    started

---
+++ app/src/main/java/com/example/fund_it/SplashActivity.kt
@@ -0,0 +1,19 @@
+package com.example.fund_it
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splash)
+        supportActionBar?.hide()
+        Handler().postDelayed({
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent);
+            finish()
+        },2000)
+    }
+}
