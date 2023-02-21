cd6290a77fcdf9a055eda4e87e5707d1e42cccd3
Vishwas <VishwasDubey150@gmail.com>
2023-02-22 02:34:30 +0530

    log in page

--- app/src/main/java/com/example/fund_it/login.kt
+++ app/src/main/java/com/example/fund_it/login.kt
@@ -2,6 +2,7 @@ package com.example.fund_it
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,4 +10,6 @@ class login : AppCompatActivity() {
         setContentView(R.layout.activity_login)
         supportActionBar?.hide()
     }
+
+    fun signup(view: View) {}
 }
